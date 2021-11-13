4197c9013b6930d1044b1286755b8f001a247591
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-13 20:00:05 +0530

    Number_of_players_playing

--- src/deckofcardspracticeproblem/DeckOfCards.java
+++ src/deckofcardspracticeproblem/DeckOfCards.java
@@ -1,9 +1,13 @@
 package deckofcardspracticeproblem;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
 	public static void main(String[] args) {
-		Displaycards rv = new Displaycards();
-		rv.displaycards();
+		Displaycards rv1 = new Displaycards();
+		rv1.displaycards();
+		players rv2 = new players();
+		rv2.numOfPlayer();
 	}
 }
 class Displaycards {
@@ -11,6 +15,7 @@ class Displaycards {
 	int[] deck = new int[52];
     String[] suits = {"Clubs", "Hearts", "Diamonds","Spades" };
     String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"};
+    
     for (int i = 0; i < deck.length; i++) {
       deck[i] = i;
     }
@@ -19,5 +24,18 @@ class Displaycards {
       String rank = ranks[deck[i] % 13];
       System.out.println( rank + " of " + suit);
     }
+    }
+}
+
+class players {
+	public void numOfPlayer(){
+        Scanner userinput = new Scanner(System.in);
+        System.out.println("Enter number of players");
+        int players = userinput.nextInt();
+        if (players < 2 || players > 4) {
+            System.out.println("Enter the players between 2 and 4");
+        }
+        else
+            System.out.println("Number of players = " +players);
     }
 }
